e1354d0fb6b52218b21abc8a45be25bad9b2623a
736863846 <77563811+736863846@users.noreply.github.com>
2021-01-24 06:04:38 +0000

    Update campus.py

--- login/campus.py
+++ login/campus.py
@@ -86,7 +86,7 @@ class CampusCard:
             password_list.append(des_3.des_3_encrypt(i, self.user_info['appKey'], '66666666'))
         login_args = {
             'appCode': 'M002',
-            'deviceId': self.user_info['863064219723915'],
+            'deviceId': '863064219723915',
             'netWork': 'wifi',
             'password': password_list,
             'qudao': 'guanwang',
